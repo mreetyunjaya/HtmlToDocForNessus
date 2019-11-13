3a2fac1c47d837c90db3b62f3cb8717910d365f8
Sebastian Wawro <user6917@gmail.com>
2019-11-13 20:03:43 +0100

    Cell highlighting

--- src/main/java/DocMaker.java
+++ src/main/java/DocMaker.java
@@ -30,6 +30,48 @@ public class DocMaker {
         return p;
     }
 
+    public static P produceHighLightedText(String text, String color) {
+        ObjectFactory factory = Context.getWmlObjectFactory();
+        P p = factory.createP();
+        R r = factory.createR();
+        Text t = factory.createText();
+        t.setValue(text);
+        r.getContent().add(t);
+        p.getContent().add(r);
+        RPr rpr = factory.createRPr();
+        BooleanDefaultTrue b = new BooleanDefaultTrue();
+        rpr.setB(b); //bold
+        //rpr.setI(b); //italic
+        //rpr.setCaps(b); //caps lock
+        Highlight myColor = factory.createHighlight();
+        myColor.setVal(color);
+        rpr.setHighlight(myColor);
+        r.setRPr(rpr);
+        return p;
+    }
+
+    public static Text produceText(String text) {
+        ObjectFactory factory = Context.getWmlObjectFactory();
+        Text t = factory.createText();
+        t.setValue(text);
+        return t;
+    }
+
+    public static TcPr generateShade(String color) {
+        ObjectFactory factory = Context.getWmlObjectFactory();
+        CTShd shd = factory.createCTShd();
+        shd.setVal(org.docx4j.wml.STShd.CLEAR);
+        //shd.setColor(color);
+        shd.setColor( "auto");
+        shd.setFill(color);
+        //shd.setThemeFill(org.docx4j.wml.STThemeColor.ACCENT_1);
+        //shd.setThemeFillTint( "66");
+
+        TcPr tcPr = new TcPr();
+        tcPr.setShd(shd);
+        return tcPr;
+    }
+
     public static void produceDoc() throws Docx4JException {
         WordprocessingMLPackage wordPackage = WordprocessingMLPackage.createPackage();
         MainDocumentPart mainDocumentPart = wordPackage.getMainDocumentPart();
@@ -83,8 +125,12 @@ public class DocMaker {
                 }*/
                 Tr tr = (Tr) row;
                 List<Object> cells = tr.getContent();
-                ((Tc)cells.get(0)).getContent().add(produceColoredText(String.valueOf(counter+1), ExploitTypeColor.typeStringToColor(exploitHolder.getType()).typeColor));
-                ((Tc)cells.get(1)).getContent().add(produceColoredText(exploitHolder.getName(), ExploitTypeColor.typeStringToColor(exploitHolder.getType()).typeColor));
+                ((Tc)cells.get(0)).getContent().add(produceColoredText(String.valueOf(counter+1), "black"));
+                ((Tc)cells.get(1)).getContent().add(produceColoredText(exploitHolder.getName(), "black"));
+                ((Tc)cells.get(0)).setTcPr(generateShade(ExploitTypeColor.typeStringToColor(exploitHolder.getType()).typeColor));
+
+                //((Tc)cells.get(0)).getContent().add(produceHighLightedText(String.valueOf(counter+1), ExploitTypeColor.typeStringToColor(exploitHolder.getType()).typeColor));
+                //((Tc)cells.get(1)).getContent().add(produceColoredText(exploitHolder.getName(), ExploitTypeColor.typeStringToColor(exploitHolder.getType()).typeColor));
                 counter++;
             }
             mainDocumentPart.getContent().add(tbl);
