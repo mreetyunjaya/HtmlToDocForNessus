c9bf7ef7d65818ef985d90147da937b43eff3818
Sebastian Wawro <user6917@gmail.com>
2019-11-13 20:30:24 +0100

    Cell width fixed; info table

--- src/main/java/DocMaker.java
+++ src/main/java/DocMaker.java
@@ -50,11 +50,15 @@ public class DocMaker {
         return p;
     }
 
-    public static Text produceText(String text) {
+    public static P produceText(String text) {
         ObjectFactory factory = Context.getWmlObjectFactory();
+        P p = factory.createP();
+        R r = factory.createR();
         Text t = factory.createText();
         t.setValue(text);
-        return t;
+        r.getContent().add(t);
+        p.getContent().add(r);
+        return p;
     }
 
     public static TcPr generateShade(String color) {
@@ -72,11 +76,47 @@ public class DocMaker {
         return tcPr;
     }
 
+    private static void setCellWidth(Tc tableCell, int width) {
+        TcPr tableCellProperties = new TcPr();
+        tableCell.setTcPr(tableCellProperties);
+    }
+
+    public static void attachInfoTable(MainDocumentPart mainDocumentPart, WordprocessingMLPackage wordPackage) {
+        mainDocumentPart.addParagraphOfText("Klasyfikacja luk");
+
+        int writableWidthTwips = wordPackage.getDocumentModel()
+                .getSections().get(0).getPageDimensions().getWritableWidthTwips();
+        int columnNumber = 4;
+        Tbl tbl = TblFactory.createTable(1, 4, writableWidthTwips/columnNumber);
+
+        List<Object> rows = tbl.getContent();
+        for (Object row : rows) {
+            Tr tr = (Tr) row;
+            List<Object> cells = tr.getContent();
+            ((Tc)cells.get(0)).getContent().clear();
+            ((Tc)cells.get(0)).getContent().add(produceText("CRITICAL"));
+            ((Tc)cells.get(0)).setTcPr(generateShade(ExploitTypeColor.CRITICAL.typeColor));
+            ((Tc)cells.get(1)).getContent().clear();
+            ((Tc)cells.get(1)).getContent().add(produceText("HIGH"));
+            ((Tc)cells.get(1)).setTcPr(generateShade(ExploitTypeColor.HIGH.typeColor));
+            ((Tc)cells.get(2)).getContent().clear();
+            ((Tc)cells.get(2)).getContent().add(produceText("MEDIUM"));
+            ((Tc)cells.get(2)).setTcPr(generateShade(ExploitTypeColor.MEDIUM.typeColor));
+            ((Tc)cells.get(3)).getContent().clear();
+            ((Tc)cells.get(3)).getContent().add(produceText("LOW"));
+            ((Tc)cells.get(3)).setTcPr(generateShade(ExploitTypeColor.LOW.typeColor));
+        }
+        mainDocumentPart.getContent().add(tbl);
+        mainDocumentPart.getContent().add(produceColoredText("", "black"));
+        mainDocumentPart.getContent().add(produceColoredText("", "black"));
+    }
+
     public static void produceDoc() throws Docx4JException {
         WordprocessingMLPackage wordPackage = WordprocessingMLPackage.createPackage();
         MainDocumentPart mainDocumentPart = wordPackage.getMainDocumentPart();
         mainDocumentPart.addStyledParagraphOfText("Title", "Nessus!");
-        mainDocumentPart.addParagraphOfText("Klasyfikacja luk");
+        attachInfoTable(mainDocumentPart, wordPackage);
+
         P emptyText = produceColoredText("", "black");
 
         for (IpInfoHolder ipInfoHolder : AppMemory.getInstance().getIpInfoList()) {
@@ -126,10 +166,11 @@ public class DocMaker {
                 Tr tr = (Tr) row;
                 List<Object> cells = tr.getContent();
                 ((Tc)cells.get(0)).getContent().clear();
-                ((Tc)cells.get(0)).getContent().add(produceColoredText(String.valueOf(counter+1), "black"));
+                ((Tc)cells.get(0)).getContent().add(produceText(String.valueOf(counter+1)+"."));
                 ((Tc)cells.get(0)).setTcPr(generateShade(ExploitTypeColor.typeStringToColor(exploitHolder.getType()).typeColor));
                 ((Tc)cells.get(1)).getContent().clear();
-                ((Tc)cells.get(1)).getContent().add(produceColoredText(exploitHolder.getName(), "black"));
+                ((Tc)cells.get(1)).getContent().add(produceText(exploitHolder.getName()));
+                setCellWidth((Tc)cells.get(1), 5000);
                 //((Tc)cells.get(0)).getContent().add(produceHighLightedText(String.valueOf(counter+1), ExploitTypeColor.typeStringToColor(exploitHolder.getType()).typeColor));
                 //((Tc)cells.get(1)).getContent().add(produceColoredText(exploitHolder.getName(), ExploitTypeColor.typeStringToColor(exploitHolder.getType()).typeColor));
                 counter++;
